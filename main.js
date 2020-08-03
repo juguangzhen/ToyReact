ad21448f0e980a5a927bdf1ec0c1bc30dddf3bb6
Guangzhen Ju 巨光振 <guangzhen.ju.o@MBC02V84E5G8WN.local>
2020-08-03 19:22:00 +0800

    day02

--- main.js
+++ main.js
@@ -13,7 +13,15 @@ class MyComponent extends Component {
 }
 
 let a = <MyComponent name='a' id='ida'>
-    <div>2222</div>
+    <div>222</div>
+    <div>333</div>
+    <div>444</div>
+    <div>
+        <ul>
+            <li>ttt</li>
+            <li>bbb</li>
+        </ul>
+    </div>
 </MyComponent>
 ToyReact.render(
     a,
